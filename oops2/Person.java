cbe1bd5bb2b11057b0779af0d047cd62a69b7aae
PrashantThakurNitP <prashant09.thakur@gmail.com>
2023-02-08 10:01:09 +0530

    theory of inheritance

--- oops2/Person.java
+++ oops2/Person.java
@@ -18,6 +18,44 @@ public class Person {
 
     }
 
+    /*
+     * Note
+     * multiple inheritance is not defined in java unlike c++
+     * class Dog extends Animal, Pet{
+     * //will not work in java mutipe inhritance add lot of complexity in c++
+     * 
+     * }
+     * 
+     * we can have chain of relationship like this in java
+     * class Animal{
+     * }
+     * class Pet Extends Animal{
+     * }
+     * class Dog Extends Pet{
+     * }
+     * Pet get class feature of Animal which itself get all property of Oject class
+     * Dog get all those feature from Pet class
+     */
+
+    /*
+     * we can have super cass refrence variable that can hold subclass reference
+     * variable
+     * Pet pet = new Dog(); //this allowed
+     * but reverse is not allowed
+     */
+
+    /*
+     * instanceof
+     * pet instanceof Dog -> true
+     * pet instanceof Animal -> true
+     * pet instance of Object -> true
+     * 
+     * Animal animal = new Animal();
+     * animal instanceof Pet -> false because animal super class and super class is
+     * not instance of subclass
+     * 
+     * animal instanceof Object -> true
+     */
     public String getName() {
         return name;
     }
